8560e368b9ab07117d1b6c06bff4765c0fd91ea0
NieHang <niehang77@gmail.com>
2025-09-28 20:23:45 +0800

    fea: finished SideBar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -11,13 +11,110 @@ export default function SideBar() {
     { src: '/sidebar/search.svg', label: 'Search Chats' },
     { src: '/sidebar/library.svg', label: 'Library' },
   ]
+  const links = [
+    {
+      src: '/sidebar/codex.svg',
+      label: 'Codex',
+      link: 'https://chatgpt.com/codex',
+    },
+    {
+      src: '/sidebar/sora.svg',
+      label: 'Sora',
+      link: 'https://sora.chatgpt.com/explore',
+    },
+    {
+      src: '/sidebar/codex.svg',
+      label: 'Codex',
+      link: 'https://chatgpt.com/codex',
+    },
+    {
+      src: '/sidebar/sora.svg',
+      label: 'Sora',
+      link: 'https://sora.chatgpt.com/explore',
+    },
+    {
+      src: '/sidebar/codex.svg',
+      label: 'Codex',
+      link: 'https://chatgpt.com/codex',
+    },
+    {
+      src: '/sidebar/sora.svg',
+      label: 'Sora',
+      link: 'https://sora.chatgpt.com/explore',
+    },
+    {
+      src: '/sidebar/codex.svg',
+      label: 'Codex',
+      link: 'https://chatgpt.com/codex',
+    },
+    {
+      src: '/sidebar/sora.svg',
+      label: 'Sora',
+      link: 'https://sora.chatgpt.com/explore',
+    },
+    {
+      src: '/sidebar/codex.svg',
+      label: 'Codex',
+      link: 'https://chatgpt.com/codex',
+    },
+    {
+      src: '/sidebar/sora.svg',
+      label: 'Sora',
+      link: 'https://sora.chatgpt.com/explore',
+    },
+    {
+      src: '/sidebar/sora.svg',
+      label: 'Sora',
+      link: 'https://sora.chatgpt.com/explore',
+    },
+    {
+      src: '/sidebar/codex.svg',
+      label: 'Codex',
+      link: 'https://chatgpt.com/codex',
+    },
+    {
+      src: '/sidebar/sora.svg',
+      label: 'Sora',
+      link: 'https://sora.chatgpt.com/explore',
+    },
+    {
+      src: '/sidebar/sora.svg',
+      label: 'Sora',
+      link: 'https://sora.chatgpt.com/explore',
+    },
+    {
+      src: '/sidebar/codex.svg',
+      label: 'Codex',
+      link: 'https://chatgpt.com/codex',
+    },
+    {
+      src: '/sidebar/sora.svg',
+      label: 'Sora',
+      link: 'https://sora.chatgpt.com/explore',
+    },
+    {
+      src: '/sidebar/sora.svg',
+      label: 'Sora',
+      link: 'https://sora.chatgpt.com/explore',
+    },
+    {
+      src: '/sidebar/codex.svg',
+      label: 'Codex',
+      link: 'https://chatgpt.com/codex',
+    },
+    {
+      src: '/sidebar/sora.svg',
+      label: 'Sora',
+      link: 'https://sora.chatgpt.com/explore',
+    },
+  ]
 
   return (
     <nav
       className={`transition-[width] duration-300 ease-in h-full p-1 border-r-1 border-gray-100 bg-gray-50`}
-      style={{ width: cutNav ? '52px' : '260px' }}
+      style={{ width: cutNav ? '45px' : '260px' }}
     >
-      <div className="flex items-center justify-end relative pb-2 h-11">
+      <div className="sticky top-0 flex items-center justify-end pb-2 h-11">
         <div
           className="sidebar-item absolute top-0 left-0"
           onClick={() => {
@@ -49,24 +146,46 @@ export default function SideBar() {
           />
         </div>
       </div>
-      {features.map((feature) => (
-        <div className="pb-2" key={feature.label}>
-          <div className="sidebar-item flex flex-row items-center w-full">
-            <Image
-              src={feature.src}
-              alt={feature.label}
-              width={20}
-              height={20}
-            />
-            <div
-              className={`text-sm ml-1 whitespace-nowrap transition-[opacity] duration-300 ease-in`}
-              style={{ opacity: cutNav ? 0 : 1 }}
-            >
-              {feature.label}
+      <aside className="sticky top-[44px] bg-gray-50">
+        {features.map((feature) => (
+          <div className="pb-2" key={feature.label}>
+            <div className={`sidebar-item flex flex-row items-center w-auto`}>
+              <Image
+                src={feature.src}
+                alt={feature.label}
+                width={20}
+                height={20}
+              />
+              <div
+                className={`text-sm ml-1 whitespace-nowrap transition-[opacity] duration-300 ease-in`}
+                style={{ opacity: cutNav ? 0 : 1 }}
+              >
+                {feature.label}
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </aside>
+      <aside className="overflow-y-auto h-[calc(100vh-112px)] pt-2 pr-1 border-t-1 border-gray-200">
+        {links.map((link, index) => (
+          <a
+            href={link.link}
+            target="_blank"
+            rel="noopener noreferrer"
+            key={index}
+          >
+            <div className="sidebar-item flex flex-row items-center w-full">
+              <Image src={link.src} alt={link.label} width={20} height={20} />
+              <div
+                className={`text-sm ml-1 whitespace-nowrap transition-[opacity] duration-300 ease-in`}
+                style={{ opacity: cutNav ? 0 : 1 }}
+              >
+                {link.label}
+              </div>
+            </div>
+          </a>
+        ))}
+      </aside>
     </nav>
   )
 }
