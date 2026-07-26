43510b842970431080a917a7686cd4702967a14e
NieHang <niehang77@gmail.com>
2026-07-26 09:20:19 +0800

    Fixed the issue that the input value wasn't cleared after sending the message

--- src/components/Form/Input/AskInput.tsx
+++ src/components/Form/Input/AskInput.tsx
@@ -217,6 +217,7 @@ export default function AskInput({
             )}
             onClick={() => {
               onKeyDown(files)
+              onChange('')
               setFiles([])
             }}
           >
